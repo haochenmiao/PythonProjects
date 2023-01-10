c63f759e72051e06cab496956c5460296b17399a
Haochen Miao <83092146+haochenmiao@users.noreply.github.com>
2023-01-10 11:04:04 -0800

    Update proj2.py

--- proj2.py
+++ proj2.py
@@ -1,61 +1,76 @@
 import numpy as np
-import scipy.linalg as la
+import matplotlib.pyplot as plt
+import scipy.io
 
-A1 = np.zeros((51, 3))
-v_1 = np.arange(0, 101, 2)
-A = np.array([[70, -20, -10], [-20, 55, -10], [-10, -10, 50]])
+# load yalefaces
+yalefaces = scipy.io.loadmat("yalefaces.mat")
+X = yalefaces["X"]
 
-for j in range(51):
-    v1 = v_1[j]
-    b = np.array([v1, 0, 200])
-    x = np.linalg.solve(A, b)
-    A1[j, :] = x
+# display the first nine images
+fig, ax = plt.subplots(3, 3, figsize=(8, 8))
+for i in range(9):
+    ax[i // 3, i % 3].imshow(X[:, i].reshape(32, 32), cmap="gray")
+    ax[i // 3, i % 3].axis("off")
+plt.show()
 
-A2 = np.zeros((51, 3))
+m = 100
+Xm = X[:,:m]
+C = Xm.T @ Xm
+A1 = C
 
-for j in range(51):
-    v1 = v_1[j]
-    b = np.array([v1, 0, 200])
-    P, L, U = la.lu(A)
-    y = np.linalg.solve(L, np.dot(P, b))
-    x = np.linalg.solve(U, y)
-    A2[j, :] = x
+score_max = 0
+r_max, c_max = None, None
+for j in range(m-1):
+    for k in range(j+1, m):
+        if C[j, k] > score_max:
+            score_max = C[j, k]
+            r_max, c_max = j, k
 
-tol = 1e-6
-A3 = np.zeros((51, 3))
+print("r_max:", r_max)
+print("c_max:", c_max)
+print("score_max:", score_max)
+A2 = [r_max, c_max]
 
-for j in range(51):
-    v1 = v_1[j]
-    I1, I2, I3 = 0, 0, 0
-    for k in range(100):
-        I1 = (v1 + 20*I2 + 10*I3)/70
-        I2 = (0 + 20*I1 + 10*I3)/55
-        I3 = (200 + 10*I1 + 10*I2)/50
-        B = [I1, I2, I3]
-        b = [I1, I2, I3]
-        Norm = np.linalg.norm(np.abs(np.array(B) - np.array(b)))
+score_min = np.inf
+r_min, c_min = None, None
+for j in range(m-1):
+    for k in range(j+1, m):
+        if C[j, k] < score_min:
+            score_min = C[j, k]
+            r_min, c_min = j, k
 
-        if Norm < tol:
-            break
-    A3[j, :] = [I1, I2, I3]
+print("r_min:", r_min)
+print("c_min:", c_min)
+print("score_min:", score_min)
+A3 = [r_min, c_min]
 
-A4 = np.zeros((51, 3))
+# Xs = X[:,image]
+image = [1, 313, 512, 5, 2400, 113, 1024, 87, 314, 2005]
+Xs = X[:, image]
 
-for j in range(51):
-    v1 = v_1[j]
-    I1, I2, I3 = 0, 0, 0
-    for k in range(100):
-        I1 = (v1 + 20*I2 + 10*I3)/70
-        I2 = (0 + 20*I1 + 10*I3)/55
-        I3 = (200 + 10*I1 + 10*I2)/50
-        B = [I1, I2, I3]
-        b = [I1, I2, I3]
-        Norm = np.linalg.norm(np.abs(np.array(B) - np.array(b)))
+# Cm = Xs.'*Xs
+Cm = np.dot(Xs.T, Xs)
+A4 = Cm
 
-        if Norm < tol:
-            break
-    A4[j, :] = [I1, I2, I3]
+# Y = X * (X.')
+Y = np.dot(X, X.T)
 
-A5 = np.zeros((1, 2))
-A5[0, 0] = np.sum(A3[:, 2])/51
-A5[0, 1] = np.sum(A4[:, 2])/51
+# [V,D] = eigs(Y,6,'lm')
+eigen_values, eigen_vectors = np.linalg.eig(Y)
+idx = eigen_values.argsort()[-6:][::-1]
+V = eigen_vectors[:, idx]
+
+# A5 = abs(V)
+A5 = np.abs(V)
+
+u, s, v = np.linalg.svd(X)
+A6 = u[:,:6]
+
+v1 = V[:,0]
+u1 = u[:,0]
+A7 = np.linalg.norm(v1-u1)
+
+singval = np.diag(s)
+lar6sing = singval[:6]
+Sum = np.sum(singval)
+A8 = 100 * np.abs([lar6sing[i]/Sum for i in range(6)])
